c82fbb787eee7ca3f9e2ecee05bc17f38cb91b04
nitin muthyala <nitinmuthyala@yahoo.com>
2017-09-18 23:38:29 +0800

    initial commit

---
+++ HappyShop/HappyShopTests/HappyShopTests.swift
@@ -0,0 +1,36 @@
+//
+//  HappyShopTests.swift
+//  HappyShopTests
+//
+//  Created by nitin muthyala on 18/9/17.
+//  Copyright © 2017 Spaceage Labs. All rights reserved.
+//
+
+import XCTest
+@testable import HappyShop
+
+class HappyShopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
